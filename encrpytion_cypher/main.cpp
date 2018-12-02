f2e4e3b0701906ae070c671155ef9fa0b9748fba
natesweiler <nateskate16@gmail.com>
2018-12-02 14:07:49 -0800

    Create main.cpp

---
+++ encrpytion_cypher/main.cpp
@@ -0,0 +1,59 @@
+//
+//  main.cpp
+//  Encrypted
+//
+//  Created by Hellen Pacheco on 11/7/18.
+//  Copyright © 2018 Hellen Pacheco. All rights reserved.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "CypherA.h"
+#include "CypherB.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+	const int MAX_TEXT = 1000;
+	string filename;
+
+	cout << "Enter file name to be decoded with algorithm A: ";
+	cin >> filename;
+
+	class CypherA messageA(filename, MAX_TEXT);
+
+	if (messageA.isEmpty())
+	{
+		cerr << "ERROR: Message could not be read." << endl;
+	}
+	else
+	{
+		messageA.decode();
+		cout << "Decoded message: \n";
+		messageA.print();
+		cout << endl << endl;;
+	}
+	cout << "Enter file name to be decoded with algorithm B: ";
+	cin >> filename;
+
+	class CypherB messageB(filename, MAX_TEXT);
+
+	if (messageB.isEmpty())
+	{
+		cerr << "ERROR: Message could not be read." << endl;
+
+	}
+	else
+	{
+		messageB.decode();
+		cout << "Decoded message: \n";
+		messageB.print();
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
